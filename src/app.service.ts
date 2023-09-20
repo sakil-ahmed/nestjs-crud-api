8bbd8a5fea05c52c0ac594344e07e982dc94376c
sakil-ahmed <sakilahmed1495@gmail.com>
2023-09-20 22:34:12 +0600

    update project module

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Navigate to Documentation - https://weary-gray-hosiery.cyclic.cloud/api/v1';
   }
 }
